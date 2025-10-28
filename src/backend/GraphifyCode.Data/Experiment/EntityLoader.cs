b1e12db3cf411248d7a0e20c8bf4ef77efaadf32
StanislavSeregin <stas_seregin@mail.ru>
2025-10-29 00:58:45 +0300

    Minor changes

--- src/backend/GraphifyCode.Data/Experiment/EntityLoader.cs
+++ src/backend/GraphifyCode.Data/Experiment/EntityLoader.cs
@@ -10,65 +10,62 @@ namespace GraphifyCode.Data.Experiment;
 
 internal static class EntityLoader
 {
-    private static readonly Loader<Service> ServicesLoader = new(
-        PathEnumerator: Directory.GetDirectories,
-        FilePathEnumerator: (path) => [Path.Combine(path, "service.md")],
-        Reader: async (path, filePath, ct) =>
-        {
-            if (filePath is { } fp
-                && Path.GetDirectoryName(fp) is { } dn
-                && Guid.TryParse(dn, out var entityId)
-                && await File.ReadAllTextAsync(fp, ct) is { } md
-                && Service.FromMarkdown(md) is { } entity)
-            {
-                entity.Id = entityId;
-                entity.Endpoints = await Load<Endpoints>(path, ct).FirstOrDefaultAsync(ct);
-                entity.UseCases = await Load<UseCase>(path, ct).ToListAsync(ct);
-                return entity;
-            }
-            else
-            {
-                return null;
-            }
-        }
-    );
-
-    private static readonly Loader<Endpoints> EndpointsLoader = new(
-        PathEnumerator: (pathContext) => [pathContext],
-        FilePathEnumerator: (path) => [Path.Combine(path, "endpoints.md")],
-        Reader: async (path, filePath, ct) => filePath is { } fp
-                && await File.ReadAllTextAsync(fp, ct) is { } md
-                && Endpoints.FromMarkdown(md) is { } entity
-            ? entity
-            : null
-    );
-
-    private static readonly Loader<UseCase> UseCasesLoader = new(
-        PathEnumerator: (pathContext) => [Path.Combine(pathContext, "usecases")],
-        FilePathEnumerator: (path) => Directory.GetFiles(path, "*.md"),
-        Reader: async (path, filePath, ct) =>
-        {
-            if (filePath is { } fp
-                && Path.GetFileNameWithoutExtension(fp) is { } fn
-                && Guid.TryParse(fn, out var id)
-                && await File.ReadAllTextAsync(fp, ct) is { } md
-                && UseCase.FromMarkdown(md) is { } entity)
-            {
-                entity.Id = id;
-                return entity;
-            }
-            else
-            {
-                return null;
-            }
-        }
-    );
-
     private static readonly Dictionary<Type, object> Loaders = new()
     {
-        [typeof(Service)] = ServicesLoader,
-        [typeof(Endpoints)] = EndpointsLoader,
-        [typeof(UseCase)] = UseCasesLoader
+        /* Services */
+        [typeof(Service)] = new Loader<Service>(
+            PathEnumerator: Directory.GetDirectories,
+            FilePathEnumerator: (path) => [Path.Combine(path, "service.md")],
+            Reader: async (path, filePath, ct) =>
+            {
+                if (filePath is { } fp
+                    && Path.GetDirectoryName(fp) is { } dn
+                    && Guid.TryParse(dn, out var entityId)
+                    && await File.ReadAllTextAsync(fp, ct) is { } md
+                    && Service.FromMarkdown(md) is { } entity)
+                {
+                    entity.Id = entityId;
+                    entity.Endpoints = await Load<Endpoints>(path, ct).FirstOrDefaultAsync(ct);
+                    entity.UseCases = await Load<UseCase>(path, ct).ToListAsync(ct);
+                    return entity;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        ),
+        /* Endpoints */
+        [typeof(Endpoints)] = new Loader<Endpoints>(
+            PathEnumerator: (pathContext) => [pathContext],
+            FilePathEnumerator: (path) => [Path.Combine(path, "endpoints.md")],
+            Reader: async (path, filePath, ct) => filePath is { } fp
+                    && await File.ReadAllTextAsync(fp, ct) is { } md
+                    && Endpoints.FromMarkdown(md) is { } entity
+                ? entity
+                : null
+        ),
+        /* UseCases */
+        [typeof(UseCase)] = new Loader<UseCase>(
+            PathEnumerator: (pathContext) => [Path.Combine(pathContext, "usecases")],
+            FilePathEnumerator: (path) => Directory.GetFiles(path, "*.md"),
+            Reader: async (path, filePath, ct) =>
+            {
+                if (filePath is { } fp
+                    && Path.GetFileNameWithoutExtension(fp) is { } fn
+                    && Guid.TryParse(fn, out var id)
+                    && await File.ReadAllTextAsync(fp, ct) is { } md
+                    && UseCase.FromMarkdown(md) is { } entity)
+                {
+                    entity.Id = id;
+                    return entity;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        )
     };
 
     public static IAsyncEnumerable<TEntity> Load<TEntity>(
